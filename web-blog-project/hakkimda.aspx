385935a0e805b7690b345ad4c283b69a520128fa
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-03-10 15:19:34 +0300

    update

--- web-blog-project/hakkimda.aspx
+++ web-blog-project/hakkimda.aspx
@@ -41,7 +41,7 @@
                 <asp:TextBox ID="TextBox7" runat="server" CssClass="form-control"></asp:TextBox>
             </div>
             <br />
-            <asp:Button ID="Button1" runat="server" Text="Güncelle" CssClass="btn btn-success" />
+            <asp:Button ID="Button1" runat="server" Text="Güncelle" CssClass="btn btn-success" OnClick="Button1_Click" />
 
         </div>
     </form>
--- web-blog-project/hakkimda.aspx.cs
+++ web-blog-project/hakkimda.aspx.cs
@@ -11,14 +11,27 @@ namespace web_blog_project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            DataSet1TableAdapters.TBL_HakkımdaTableAdapter hakkımdaTablosu = new DataSet1TableAdapters.TBL_HakkımdaTableAdapter();
-            TextBox1.Text = hakkımdaTablosu.HakkımdaListele()[0].AD;
-            TextBox2.Text = hakkımdaTablosu.HakkımdaListele()[0].SOYAD;
-            TextBox3.Text = hakkımdaTablosu.HakkımdaListele()[0].ADRES;
-            TextBox4.Text = hakkımdaTablosu.HakkımdaListele()[0].MAIL;
-            TextBox5.Text = hakkımdaTablosu.HakkımdaListele()[0].TELEFON;
-            TextBox6.Text = hakkımdaTablosu.HakkımdaListele()[0].KISANOT;
-            TextBox7.Text = hakkımdaTablosu.HakkımdaListele()[0].FOTOGRAF;
+            if (Page.IsPostBack == false)
+            {
+                //PostBack kullanarak sadece sayfa ilk kez yüklendiği zaman veri tabanından bilgileri almış oluyor.
+                //Aşağıda verileri güncellediğimiz zaman güncellenen veriler gözükecektir.
+
+                DataSet1TableAdapters.TBL_HakkımdaTableAdapter hakkımdaTablosu = new DataSet1TableAdapters.TBL_HakkımdaTableAdapter();
+                TextBox1.Text = hakkımdaTablosu.HakkımdaListele()[0].AD;
+                TextBox2.Text = hakkımdaTablosu.HakkımdaListele()[0].SOYAD;
+                TextBox3.Text = hakkımdaTablosu.HakkımdaListele()[0].ADRES;
+                TextBox4.Text = hakkımdaTablosu.HakkımdaListele()[0].MAIL;
+                TextBox5.Text = hakkımdaTablosu.HakkımdaListele()[0].TELEFON;
+                TextBox6.Text = hakkımdaTablosu.HakkımdaListele()[0].KISANOT;
+                TextBox7.Text = hakkımdaTablosu.HakkımdaListele()[0].FOTOGRAF;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBL_HakkımdaTableAdapter dt1 = new DataSet1TableAdapters.TBL_HakkımdaTableAdapter();
+            dt1.HakkimdaGuncelle(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text, TextBox6.Text, TextBox7.Text);
+            Response.Redirect("WebForm1.Aspx");
         }
     }
 }
